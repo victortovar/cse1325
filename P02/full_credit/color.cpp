64c1406b6dc317bea02798f888d7f0226ed01643
victor.tovar <victor.tovar@mavs.uta.edu>
2020-02-03 23:37:32 -0600

    color.cpp

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -10,15 +10,7 @@ std::string Color::to_string(){
 }
 
 std::string Color::colorize(std::string text){
-	std::string colorString, newString;
-	for(int i=0;i<text.size();i++)
-	{
-	   if(text[i]==',') text.replace(i,1,";");
-	}
-	for(int i=1;i<text.size()-1;i++)
-	{
-	   newString[i-1]=text[i];
-	}
+	std::string colorString;
 	colorString="\033[38;2;"+std::to_string(_red)+";"+std::to_string(_green)+";"+std::to_string(_blue)+";177m"+text+"\033[0m";
 	return colorString;
 }
